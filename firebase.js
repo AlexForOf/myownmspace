5d6ab7262578b715be0c4ad4702b1ffa1bb90f8a
AlexForOf <aleshmindr@gmail.com>
2024-02-10 13:22:04 +0100

    Base setup of generic Vite+React application, also including Firebase hosting

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJCFua__MApRWhZYI2KEV8RBb-86CltY0",
+  authDomain: "myownmspace.firebaseapp.com",
+  projectId: "myownmspace",
+  storageBucket: "myownmspace.appspot.com",
+  messagingSenderId: "109000307754",
+  appId: "1:109000307754:web:b1bc59c6e025a913b9bf53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
